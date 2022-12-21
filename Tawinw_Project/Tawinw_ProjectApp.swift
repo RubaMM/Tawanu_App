7dafd6e5ceacc8b21880d2d9a95acd54d4224083
Insreen <nmalfaifi01@gmail.com>
2022-12-22 01:17:57 +0300

    Initial Commit

---
+++ Tawinw_Project/Tawinw_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tawinw_ProjectApp.swift
+//  Tawinw_Project
+//
+//  Created by Nsreen Alfaifi on 28/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct Tawinw_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tawinw_ProjectUITests/Tawinw_ProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Tawinw_ProjectUITestsLaunchTests.swift
+//  Tawinw_ProjectUITests
+//
+//  Created by Nsreen Alfaifi on 28/05/1444 AH.
+//
+
+import XCTest
+
+final class Tawinw_ProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
